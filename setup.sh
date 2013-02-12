bfe1fabb88e3cd9a002c5058536651ee961b72d7
Keisuke SATO <riaf@me.com>
2013-02-12 10:44:16 +0900

    js setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ln -s "$PWD/Midtown.css" ~/Library/Application\ Support/net.limechat.LimeChat-AppStore/Themes/Midtown.css
+ln -s "$PWD/Midtown.js" ~/Library/Application\ Support/net.limechat.LimeChat-AppStore/Themes/Midtown.js
 ln -s "$PWD/Midtown.yaml" ~/Library/Application\ Support/net.limechat.LimeChat-AppStore/Themes/Midtown.yaml
 
